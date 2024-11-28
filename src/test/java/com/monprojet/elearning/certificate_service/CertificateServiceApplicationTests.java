89e65926b008d66b3500e32494cb5268c6cd9708
Karim <boualam.karim94@gmail.com>
2024-11-28 01:12:34 +0100

    Initialisation du service Certification

---
+++ src/test/java/com/monprojet/elearning/certificate_service/CertificateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monprojet.elearning.certificate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
